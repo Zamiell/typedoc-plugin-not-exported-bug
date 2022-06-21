db2fe3788cfc67b4ed2fc79db5f7f07ea6169921
James <5511220+Zamiell@users.noreply.github.com>
2022-06-21 09:14:09 -0400

    Update someFile.ts

--- src/someFile.ts
+++ src/someFile.ts
@@ -1,15 +1,11 @@
 /**
  * This is thing 1.
  */
-export const thing1 = {
-  foo: 0,
-}
+export const thing1 = {};
 
 /**
  * This is thing 2.
  *
  * @notExported
  */
-const thing2 = {
-  foo: 0,
-};
+const thing2 = {};
